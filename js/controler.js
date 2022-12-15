34dea68cce0fb033c25978ae542f7b02ff58619d
OliaK <olgamr@walla.com>
2022-12-15 13:44:08 +0200

    adding color options

--- js/controler.js
+++ js/controler.js
@@ -19,23 +19,11 @@ function onInit() {
 }
 
 
-function renderGallery() {
-    var images = getImgs()
-    var strHtmls = images.map(function (img) {
-        return `  
-        <img class="meme-image" src="${img.url}" id="${img.id}" onclick="onCreateMeme('${img.id}')">
-       `
-    })
-    document.querySelector('.img-container').innerHTML = strHtmls.join('');
-}
-
-
-
 function onCreateMeme(imgId) {
     setPage()
     getSelectedImg(imgId)
     var currImgId = getCurrImgId()
-    onDisplayMeme(currImgId)
+    renderMeme(currImgId)
     renderCanvas()
 }
 
@@ -43,14 +31,15 @@ function onCreateMeme(imgId) {
 function renderCanvas() {
     gCanvas = document.querySelector('#meme-canvas')
     gCtx = gCanvas.getContext('2d')
-    onDisplayMeme()
+    renderMeme()
+    onChangeTextSize
 }
 
 // function renderImg(img) {
 //     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
 // }
 
-function onDisplayMeme(imgId) {
+function renderMeme(imgId) {
     var meme = getMeme()
     var txt = meme.lines[0].txt
     var meme = new Image()
@@ -68,7 +57,7 @@ function onsendInput(elTxt) {
     console.log(elTxt.value)
 
     drawText(elTxt.value, 30, 50)
-    onDisplayMeme()
+    renderMeme()
 }
 
 
@@ -93,52 +82,38 @@ function setPage() {
     document.querySelector('.canvas-editors').style.display = 'block'
 }
 
+function onChangeTextSize(num) {
 
-
-//To keep track of the link we are on
-const activePage = window.location.pathname
-const navLinks = document.querySelectorAll('a').forEach(link => {
-    if (link.href.includes(`${activePage}`)) {
-        link.classList.add('active')
-    }
-})
-
-//TO DO - FILTER
-function onFilterMemes(val) {
-    if (val === 'Funny') console.log('Funny')
-    if (val === 'Animal') console.log('Animal')
-    if (val === 'Bad') console.log('Bad')
-    if (val === 'Funny') console.log('Funny')
-    if (val === 'Funny') console.log('Funny')
+    changeSize(num)
+    renderCanvas()
 
 }
 
-//TO DO - SET-lANG
-function onSetLang(lang) {
-    if (lang === 'HE') console.log('he')
-    if (lang === 'EN') console.log('en')
-   
+function onChangeTextClor(color) {
+    editMeme('innerColor', color)
+    renderMeme()
+    
+}
+
+function onChangeTextColor(clr) {
+    // setTextColor()
+    renderCanvas()
+    
 }
 
 
+function onChangeStrokeColor(color) {
+    editMeme('strColor', color)
+    renderCanvas()
 
-function OnGetEmailLink() {
-    var userEmailInput = document.querySelector('[name="user-email"]').value
-    var userSubInput = document.querySelector('[name="user-subject"]').value
-    var userMsgInput = document.querySelector('[name="massage"]').value
-    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${userEmailInput}.com&su=${userSubInput}&body=${userMsgInput}`)
 }
 
 
-
-
-function toggleMenu() {
-    document.body.classList.toggle('menu-open')
-}
 
 function onAddNewLine() {
     document.querySelector('.userTxt').value = ''
     addLine()
     renderCanvas()
-    
+
 }
+
--- js/services.js
+++ js/services.js
@@ -1,6 +1,24 @@
 'use strict'
 
 var gMemes = []
+var gIdx = 0
+
+var gKeyWords ={
+    'funny':12,
+    'cat': 1,
+    'happy': 4,
+    'actor': 4,
+    'politician': 3,
+    'cute': 12,
+    'animals': 3,
+    'cartoon': 1,
+    'sarcasm': 7,
+    'geeky': 2,
+    'scary': 1,
+    'sports': 2
+
+}
+
 var gImages = [
     {
         id: 1,
@@ -10,7 +28,7 @@ var gImages = [
     {
         id: 2,
         url: 'meme-imgs (square)/2.jpg',
-        keyWords: ['cute', 'animals']
+        keyWords: ['cute', 'animals','happy']
     },
     {
 
@@ -53,7 +71,7 @@ var gImages = [
         
         id: 10,
         url: 'meme-imgs (square)/10.jpg',
-        keyWords: ['politicians', 'funny', 'sarcasm']
+        keyWords: ['politicians', 'funny', 'happy']
     },
     {
         id: 11,
@@ -95,7 +113,7 @@ var gImages = [
     {
         id: 18,
         url: 'meme-imgs (square)/18.jpg',
-        keyWords: ['anime', 'funny']
+        keyWords: ['cartoon', 'funny']
 
     }
    
@@ -104,46 +122,67 @@ var gImages = [
 
 var gMeme = {
     selectedImgId: 5,
-    selectedLineIdx: 0,
+    selectedLineIdx: "0",
     lines: [
         {
-            txt: 'Iv sometimes eat falafel',
+            txt: 'I sometimes eat falafel',
             size: 20,
             align: 'center',
-            innerColor: 'black',
+            innerColor: 'white',
             strColor:'black',
 
         },
-        // {
-        //     txt: 'Try cabbage ',
-        //     size: 20,
-        //     align: 'left',
-        //     color: 'red'
-        // }
+        {
+            txt: 'Try more ',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
     ]
 }
 
 
-function addLine() {
-    var line = {
-        txt: "Add some new text here",
-        size : 20,
-        align :'center',
-        font: 'impact',
-        innerColor: 'black',
-        strColor: "white"
+// function addLine() {
+//     var line = {
+//         txt: "Try more",
+//         size : 20,
+//         align :'center',
+//         font: 'impact',
+//         innerColor: 'black',
+//         strColor: "white"
+
+//     }
+//     gMeme.lines.push(line)
+//     gMeme.selectedLineIdx++
+//     console.log('new line is line:', line);
+    
+// }
+
+//set gMeme
 
 
+function changeSize(num) {
+    if (gMeme.lines.length === 0) return
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].size += num
 
-    }
-    gMeme.lines.push(line)
-    gMeme.selectedLineIdx++
-    console.log('new line is line:', line);
+}
+
+function editMeme(key, value) {
+    if(gMeme.lines.length === 0) return
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx][key] = value
+    
+}
+
+function getCurrImg() {
+    let currImg = gImages.find((img) => img.id === gMeme.selectedImgId)
+    return currImg.url
     
 }
 
 function getImgById() {
-    console.log(img)
+   
     var img = gImages.find((img) => img.id === gMeme.selectedImgId)
     return img
 }
